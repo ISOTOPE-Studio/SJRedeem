131e623fe3cb8ba84a7ebc2ac7a6f5dc4df1c605
TANJX <327060609@qq.com>
2016-02-06 10:42:57 +0800

    First Release 1.0

--- src/main/java/cc/isotopestudio/SJRedeem/sjredeem/SJRedeemCommand.java
+++ src/main/java/cc/isotopestudio/SJRedeem/sjredeem/SJRedeemCommand.java
@@ -6,10 +6,12 @@ import java.util.Set;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 
 public class SJRedeemCommand implements CommandExecutor {
 	private final SJRedeem plugin;
@@ -21,6 +23,43 @@ public class SJRedeemCommand implements CommandExecutor {
 	public int generateRedeem() {
 		int temp = (int) (Math.random() * 900000 + 100000);
 		return temp;
+	}
+
+	public int getNumber(String args, int size) {
+		// Get Number
+		int redeem = 0;
+		ArrayList<Integer> number = new ArrayList<Integer>();
+		for (int i = 0; i < args.length(); i++) {
+			char temp = args.charAt(i);
+			if (temp < '0' || temp > '9') {
+				return -1;
+			}
+			number.add(temp - '0');
+		}
+		if (size != -1)
+			if (number.size() != size) {
+				return -1;
+			}
+		for (int i = number.size() - 1; i >= 0; i--) {
+			int digit = number.size() - i - 1;
+			redeem += Math.pow(10, digit) * number.get(i);
+		}
+		return redeem;
+	}
+
+	public ItemStack getItemStack(String arg, String num) {
+		int itemNumber = getNumber(arg, -1);
+		ItemStack item = null;
+		if (itemNumber != -1) {
+			item = new ItemStack(itemNumber, Integer.parseInt(num));
+		} else {
+			try {
+				item = new ItemStack(Material.getMaterial(arg), Integer.parseInt(num));
+			} catch (Exception e) {
+				item = null;
+			}
+		}
+		return item;
 	}
 
 	@Override
@@ -52,28 +91,14 @@ public class SJRedeemCommand implements CommandExecutor {
 					return true;
 				}
 
-				if (args.length == 2) {
+				if (args.length <= 2) {
 
 					// Get Number
-					int redeem = 0;
-					ArrayList<Integer> number = new ArrayList<Integer>();
-					for (int i = 0; i < args[0].length(); i++) {
-						char temp = args[0].charAt(i);
-						if (temp < '0' || temp > '9') {
-							sender.sendMessage((new StringBuilder(plugin.prefix)).append(ChatColor.RED)
-									.append("这不是一个有效的兑换码").toString());
-							return true;
-						}
-						number.add(temp - '0');
-					}
-					if (number.size() != 6) {
+					int redeem = getNumber(args[0], 6);
+					if (redeem == -1) {
 						sender.sendMessage((new StringBuilder(plugin.prefix)).append(ChatColor.RED)
 								.append("这不是一个有效的兑换码").toString());
 						return true;
-					}
-					for (int i = number.size() - 1; i >= 0; i--) {
-						int digit = number.size() - i - 1;
-						redeem += Math.pow(10, digit) * number.get(i);
 					}
 
 					List<String> list = plugin.getPlayersData().getStringList("Players." + player.getName());
@@ -82,33 +107,74 @@ public class SJRedeemCommand implements CommandExecutor {
 						String tempRedeem = redeemString.substring(0, 6);
 						if (tempRedeem.equals("" + redeem)) {
 							String timesString = redeemString.substring(7);
-							// Get Number
-							int times = 0;
-							number = new ArrayList<Integer>();
-							for (int i = 0; i < timesString.length(); i++) {
-								char temp = timesString.charAt(i);
-								number.add(temp - '0');
-							}
-							for (int i = number.size() - 1; i >= 0; i--) {
-								int digit = number.size() - i - 1;
-								times += Math.pow(10, digit) * number.get(i);
-							}
-							sender.sendMessage((new StringBuilder(plugin.prefix)).append(ChatColor.AQUA).append("成功领取")
-									.toString());
 
-							if (times <= 1) {
+							int times = getNumber(timesString, -1);
+							List<String> itemList = plugin.getConfig().getStringList("Items");
+							ItemStack[] itemStacks = new ItemStack[itemList.size()];
+							int j = 0;
+							for (String itemLine : itemList) {
+								String split[] = itemLine.split(" ");
+								itemStacks[j] = getItemStack(split[0], split[1]);
+								if (itemStacks[j] == null) {
+									itemStacks[j] = new ItemStack(Material.AIR, 1);
+									sender.sendMessage((new StringBuilder(plugin.prefix)).append(ChatColor.RED)
+											.append("配置文件出错，请联系管理员").toString());
+								}
+								j++;
+							}
+
+							if (args.length == 2) {
+
+								ItemStack item = getItemStack(args[1], "1");
+								if (item == null) {
+									sender.sendMessage((new StringBuilder(plugin.prefix)).append(ChatColor.RED)
+											.append("物品不存在").toString());
+									return true;
+								}
+								for (ItemStack temp : itemStacks) {
+									if (temp.getType().equals(item.getType())) {
+										if (player.getInventory().firstEmpty() == -1) {
+											sender.sendMessage((new StringBuilder(plugin.prefix)).append(ChatColor.RED)
+													.append("背包满了？").toString());
+											return true;
+										}
+										player.getInventory().addItem(temp);
+										sender.sendMessage((new StringBuilder(plugin.prefix)).append(ChatColor.AQUA)
+												.append("成功领取" + temp.getType() + " " + temp.getAmount() + "个")
+												.toString());
+										if (times <= 1) {
+											sender.sendMessage((new StringBuilder(plugin.prefix)).append(ChatColor.RED)
+													.append("兑换码使用次数已到").toString());
+											list.remove(index);
+											plugin.getPlayersData().set("Players." + player.getName(), list);
+											plugin.savePlayersData();
+											return true;
+										}
+										list.remove(index);
+										list.add(redeem + " " + (times - 1));
+										plugin.getPlayersData().set("Players." + player.getName(), list);
+										plugin.savePlayersData();
+
+										return true;
+									}
+								}
 								sender.sendMessage((new StringBuilder(plugin.prefix)).append(ChatColor.RED)
-										.append("兑换码使用次数已到").toString());
-								list.remove(index);
-								plugin.getPlayersData().set("Players." + player.getName(), list);
-								plugin.savePlayersData();
+										.append("物品不存在").toString());
+								return true;
+							} else {
+								sender.sendMessage(
+										(new StringBuilder(plugin.prefix).append(ChatColor.GRAY).append(ChatColor.BOLD)
+												.append("" + redeem).append(ChatColor.RESET).append(ChatColor.BLUE)
+												.append(" 可用 " + times + " 次，每次下面列表中的其中一个：").toString()));
+								for (ItemStack temp : itemStacks) {
+									sender.sendMessage((new StringBuilder("    ").append(ChatColor.AQUA)
+											.append(ChatColor.BOLD).append(temp.getType()).append(ChatColor.GRAY)
+											.append("(" + temp.getTypeId() + ")").append(ChatColor.AQUA)
+											.append(ChatColor.BLUE).append(" " + temp.getAmount() + " 个").toString()));
+
+								}
 								return true;
 							}
-							list.remove(index);
-							list.add(redeem + " " + (times - 1));
-							plugin.getPlayersData().set("Players." + player.getName(), list);
-							plugin.savePlayersData();
-							return true;
 						}
 						index++;
 					}
@@ -218,6 +284,13 @@ public class SJRedeemCommand implements CommandExecutor {
 					}
 				}
 
+				if (args[0].equals("reload")) {
+					plugin.onReload();
+					sender.sendMessage(
+							(new StringBuilder(plugin.prefix)).append(ChatColor.AQUA).append("成功重载配置文件").toString());
+					return true;
+				}
+
 				if (args[0].equals("about")) {
 					about(sender);
 					return true;
@@ -275,6 +348,9 @@ public class SJRedeemCommand implements CommandExecutor {
 				sender.sendMessage(
 						(new StringBuilder()).append(ChatColor.GOLD).append("/sda list").append(ChatColor.GRAY)
 								.append(" - ").append(ChatColor.LIGHT_PURPLE).append("查看兑换码列表及其信息").toString());
+				sender.sendMessage(
+						(new StringBuilder()).append(ChatColor.GOLD).append("/sda reload").append(ChatColor.GRAY)
+								.append(" - ").append(ChatColor.LIGHT_PURPLE).append("重新载入配置文件").toString());
 				sender.sendMessage(
 						new StringBuilder().append(ChatColor.GOLD).append("/sda about").append(ChatColor.GRAY)
 								.append(" - ").append(ChatColor.LIGHT_PURPLE).append("查看插件信息").toString());
